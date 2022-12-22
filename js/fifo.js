7f47a9dd6fdb0aac44a2e0339e14baf2876de351
Gerardo Alberto Soto Alvarez del Castillo <gerardo.8.soto@gmail.com>
2022-12-22 14:20:33 -0600

    fifo updated but updateForm function isn't working

--- js/fifo.js
+++ js/fifo.js
@@ -17,34 +17,34 @@ class Queue {
 		// check if the queue is empty
 		if (this.front === this.end) {
 			return null;
-		}
+		};
 
 		// delete the first value in the Queue
 		const data = this.items[this.front];
 		delete this.items[this.front];
 		this.front++;
 		return data;
-	}
+	};
 
 	getSize() {
 		return this.end - this.front;
-	}
+	};
 
 	isEmpty() {
 		if (this.getSize() === 0) {
 			return true;
 		} else {
 			return false;
-		}
-	}
+		};
+	};
 
 	peek() {
 		if (this.getSize() === 0) {
 			return null;
 		} else {
 			return this.items[this.front];
-		}
-	}
+		};
+	};
 
 	print() {
 		if (this.getSize() === 0) {
@@ -54,12 +54,12 @@ class Queue {
 			for (let index = this.front; index < this.end; index++) {
 				result += this.items[index] + ', ';
 				
-			}
+			};
 			
 			return result;
-		}
-	}
-}
+		};
+	};
+};
 
 
 // input
@@ -90,12 +90,18 @@ function btnEnqueueInput() {
 	queueEmpty.value = isEmpty;
 
 	let result = queue.print();
+	//console.log(result);
 	outputQueue.value = result;
-}
+};
 
 function btnDequeueInput() {
 	let data = queue.dequeue();
-	dequeueOutput.value = data;
+
+	if (data == null) {
+		dequeueOutput.value = 'Queue empty.';
+	} else {
+		dequeueOutput.value = data;
+	};
 
 	let size = queue.getSize();
 	queueSize.value = size;
@@ -104,20 +110,48 @@ function btnDequeueInput() {
 	queueEmpty.value = isEmpty;
 
 	let result = queue.print();
+	//console.log(result);
 	outputQueue.value = result;
-}
+};
 
 function btnQueuePeek() {
 	let data = queue.peek();
-	queuePeek.value = data;
-}
+	
+	if (data == null) {
+		queuePeek.value = 'Queue empty';
+	} else {
+		queuePeek.value = data;
+	};
+
+	let size = queue.getSize();
+	queueSize.value = size;
+
+	let isEmpty = queue.isEmpty();
+	queueEmpty.value = isEmpty;
+
+	let result = queue.print();
+	//console.log(result);
+	outputQueue.value = result;
+};
+
+function updateForm() {
+	let size = queue.getSize();
+	queueSize.value = size;
+
+	let isEmpty = queue.isEmpty();
+	queueEmpty.value = isEmpty;
+
+	let result = queue.print();
+	//console.log(result);
+	outputQueue.value = result;
+};
+
 
 enqueueData.addEventListener('keydown', ({key}) => {
 	if (key === "Enter") {
 		btnEnqueueInput();
-	}
+	};
 });
 
 // create a queue object
 const queue = new Queue();
-
